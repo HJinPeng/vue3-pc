7ce1bc31c49aef62b976724aecf74b3bbe1460f9
jinpengh <742463015@qq.com>
2023-11-12 22:00:29 +0800

    feat: :sparkles: 添加异常页面

--- src/views/exceptions/NoPermission.vue
+++ src/views/exceptions/NoPermission.vue
@@ -1,3 +1,38 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
+
 <template>
-  <div>1</div>
+  <div cls="not-permission">
+    <p cls="tip">没有权限访问该资源，请联系管理员</p>
+    <a-button type="primary" @click="router.go(-1)">返回上一页</a-button>
+  </div>
 </template>
+
+<style lang="less" module>
+.not-permission {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  .tip {
+    font-size: 20px;
+  }
+  &::before {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    font-size: 300px;
+    font-weight: bold;
+    color: #ebebeb;
+    text-align: center;
+    content: '403';
+  }
+}
+</style>
--- src/views/exceptions/NotFound.vue
+++ src/views/exceptions/NotFound.vue
@@ -1,3 +1,38 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
+
 <template>
-  <div>1</div>
+  <div cls="not-found">
+    <p cls="tip">页面不存在</p>
+    <a-button type="primary" @click="router.go(-1)">返回上一页</a-button>
+  </div>
 </template>
+
+<style lang="less" module>
+.not-found {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  .tip {
+    font-size: 20px;
+  }
+  &::before {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    font-size: 300px;
+    font-weight: bold;
+    color: #ebebeb;
+    text-align: center;
+    content: '404';
+  }
+}
+</style>
